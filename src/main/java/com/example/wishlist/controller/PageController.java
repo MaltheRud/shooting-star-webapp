6cb0689d9de39556fe9395674aa351c76003c3a3
R4X05Z3R0 <razeroeuw@gmail.com>
2026-04-16 17:09:25 +0200

    Populated all HTML files and stuff

--- src/main/java/com/example/wishlist/controller/PageController.java
+++ src/main/java/com/example/wishlist/controller/PageController.java
@@ -1,6 +1,5 @@
 package com.example.wishlist.controller;
 
-import com.example.wishlist.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/com/example/wishlist/controller/WishListController.java
+++ src/main/java/com/example/wishlist/controller/WishListController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/shootingstar")
+@RequestMapping("/shootingstar/wishlist")
 public class WishListController {
     private final WishListService service;
 
