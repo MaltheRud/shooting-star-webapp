0c9bbc8dd346ff1012ee801225292390cd3b567a
R4X05Z3R0 <razeroeuw@gmail.com>
2026-04-16 19:26:29 +0200

    Signup, Login and Session created. Auth controller added for login in check and session handling

---
+++ src/main/java/com/example/wishlist/controller/AuthController.java
@@ -0,0 +1,40 @@
+package com.example.wishlist.controller;
+
+import com.example.wishlist.model.User;
+import com.example.wishlist.service.UserService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/auth")
+public class AuthController {
+    private UserService service;
+
+    public AuthController(UserService service){
+        this.service = service;
+    }
+
+
+    @PostMapping("/login")
+     public String login(@RequestParam("username") String username, @RequestParam("password") String password,
+                         HttpSession session, Model model){
+
+        try {
+            User user = service.userLogin(username, password);
+            session.setAttribute("userId", user.getUserId());
+            session.setAttribute("username", user.getUsername());
+            session.setMaxInactiveInterval(20);
+
+        return "redirect:/dashboard";
+
+        } catch (Exception e){
+            model.addAttribute("wronglogin","Invalid Username/Password");
+            return "login";
+        }
+
+    }
+}
--- src/main/java/com/example/wishlist/controller/UserController.java
+++ src/main/java/com/example/wishlist/controller/UserController.java
@@ -4,12 +4,14 @@ import com.example.wishlist.model.User;
 import com.example.wishlist.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/users")
+@Controller
+@RequestMapping("/shootingstar/users")
 public class UserController {
     private final UserService service;
 
@@ -18,9 +20,9 @@ public class UserController {
     }
 
     @PostMapping("/create")
-    public ResponseEntity<Integer> createUser(@RequestBody User user){
-        Integer createdUser = service.createUser(user);
-        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    public String createUser(@ModelAttribute User user){
+        service.createUser(user);
+        return "redirect:/shootingstar/login";
     }
 
     @GetMapping("/list")
